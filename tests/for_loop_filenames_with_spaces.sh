1aa61ce5bbff73b31720b308ceff21f34ccf5cf0
Brian Manning <elspicyjack at gmail dot com>
2011-01-11 16:45:22 -0800

    found a webpage and a working solution for filenames with spaces in them

--- tests/for_loop_filenames_with_spaces.sh
+++ tests/for_loop_filenames_with_spaces.sh
@@ -3,15 +3,21 @@
 # looping over a list of files that contain spaces; how can you get around the
 # shell interpreting the spaces in filenames as a shell delimter
 
+# ideas:
+# http://www.macgeekery.com/tips/cli/handling_filenames_with_spaces_in_bash
+
 test_function () {
-    local FILES="${1}"
-    echo "received: $FILES"
+    local FILE="${1}"
+    #local FILE="${@}"
+    echo "received: ${FILE}"
+    #echo "received: ${@}"
 
     #for FILE in $(IFS="\0" echo ${FILES});
-    for FILE in $(IFS='|' echo ${FILES});
-    do
+    #for FILE in $(IFS='|' echo ${FILES});
+    #for FILE in "$@";
+    #do
         ls -ld "$FILE"
-    done
+    #done
 } # test_function ()
 
 TEMP_DIR=$(/bin/mktemp -d /dev/shm/filenames_test.XXXXX)
@@ -20,8 +26,13 @@ do
     touch "${TEMP_DIR}/test file number ${FILE_NUM}"
 done
 #FILE_LIST=$(ls -1 ${TEMP_DIR} | tr '\n' '\0')
-FILE_LIST=$(ls -1 ${TEMP_DIR} | tr '\n' '|')
-test_function "${FILE_LIST}"
+#FILE_LIST=$(ls -1 ${TEMP_DIR} | tr '\n' '|')
+#find $TEMP_DIR -name '* *' -print0 | xargs -0 test_function
+#find $TEMP_DIR -name '* *' -print0 | xargs -0 -n 1 -I % echo % rocks 
+find $TEMP_DIR -name '* *' | while read FILE
+do 
+    test_function "${FILE}"
+done
 
 /bin/rm -rf $TEMP_DIR
 exit 0
