cba2878e9f0d91a84ee857929fb016d91d595701
Brian Manning <brian at portaboom dot com>
2011-07-27 23:52:43 -0700

    added creation of the metadata_never_index file in the clean_mac_garbage script

--- clean_mac_garbage.sh
+++ clean_mac_garbage.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# creating this file causes spotlight to skip indexing this disk
+touch .metadata_never_index
+# delete the gunk
 rm -rf ._.Trashes
 rm -rf .fseventsd
 rm -rf .Spotlight-V100
