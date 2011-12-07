b14ddb29866540fb279e881052650bd6dfe5b289
Brian Manning <brian at portaboom dot com>
2011-12-06 17:18:31 -0800

    added functions to icinga_install.sh
    - added prereqs and install_icinga functions; this should make it easier to
      debug individual build steps

--- icinga_install.sh
+++ icinga_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # script variables
 CFG_PKG="icinga"
@@ -6,35 +6,85 @@ CFG_DIR=/opt/${CFG_PKG}
 ICINGA_BUILD_DIR="/usr/local/src/icinga"
 SF_BASE="http://sourceforge.net/projects/icinga/files"
 
+### SCRIPT FUNCTIONS ###
+show_banner () {
+    show_banner_TEXT="$*"
+    echo "############# ${show_banner_TEXT} #############"
+} # show_banner
+
+### BEGIN MAIN SCRIPT ###
+
+## PREREQS FUNCTION ##
+prereqs () {
     # create an icinga user
     # /usr/sbin/adduser --gid XXX --uid XXX --home XXX --disabled-login icinga
+    show_banner "Adding 'icinga' user"
     /usr/sbin/useradd --system --create-home icinga
 
     # install prerequisites
+    show_banner "Installing prerequisite packages"
     apt-get --assume-yes install apache2 build-essential libgd2-xpm-dev \
         libjpeg62 libjpeg62-dev libpng12-0 libpng12-dev snmp libsnmp-base \
-        libdbi0 libdbi0-dev libssl-dev mysql-client
+        libdbi0 libdbi0-dev libssl-dev mysql-client libperl-dev
 
     # create and change to the build directory
+    show_banner "Creating build directory"
     mkdir -p $ICINGA_BUILD_DIR
     cd ${ICINGA_BUILD_DIR}
+} # prereqs
 
+## INSTALL_ICINGA FUNCTION ##
+install_icinga () {
     # icinga
-    VERSION=1.6.1
+    local VERSION="1.6.1"
+    local START_DIR=$PWD
+
     if [ ! -e "icinga-${VERSION}.tar.gz" ]; then
+        show_banner "Dowloading icinga version $VERSION"
         wget -O icinga-${VERSION}.tar.gz \
         ${SF_BASE}/icinga/${VERSION}/icinga-${VERSION}.tar.gz/download
     fi
     if [ -d "icinga-${VERSION}" ]; then
+        show_banner "Removing old copy of icinga"
         rm -rf "icinga-${VERSION}"
     fi
+    show_banner "Unpacking icinga version ${VERSION}"
     tar -zxvf icinga-${VERSION}.tar.gz
+
     cd icinga-${VERSION}
+    show_banner "Running './configure'"
     ./configure --prefix=/usr/local/share/icinga --enable-idoutils \
     --enable-nanosleep --enable-ssl --with-perlcache --enable-embedded-perl
-    time make all
+    show_banner "Running 'make all'"
+    /usr/bin/time make all
+    show_banner "Running 'make install'"
+    /usr/bin/time make install
+    show_banner "Running 'make install-idoutils'"
+    /usr/bin/time make install-idoutils
+    show_banner "Running 'make install-config'"
+    /usr/bin/time make install-config
+    show_banner "Running 'make install-webconf'"
+    /usr/bin/time make install-webconf
+    # FIXME move the resulting file from /etc/apache2/conf.d to
+    # /etc/apache2/sites-available
+    show_banner "Moving config file to sites-available"
+    if [ ! -e /etc/apache2/sites-available/icinga ]; then
+        /bin/mv /etc/apache2/conf.d/icinga.conf \
+            /etc/apache2/sites-available/icinga
+    else
+        echo "WARNING: /etc/apache2/sites-available/icinga file exists"
+        echo "WARNING: Will not replace existing file"
+        echo "WARNING: Copy the file /etc/apache2/conf.d/icinga.conf"
+        echo "WARNING: to /etc/apache2/sites-available/icinga if you"
+        echo "WARNING: want to use the default Icinga Apache config file"
+    fi
+
+    # from apache2.2-common
+    show_banner "Enabling icinga site via sites-enabled/a2ensite"
+    /usr/sbin/a2ensite icinga
     cd ..
-exit 0
+
+
     # icinga-web
     VERSION=1.6.0
     if [ ! -e "icinga-web-${VERSION}.tar.gz" ]; then
