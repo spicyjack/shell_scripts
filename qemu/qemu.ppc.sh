6cdc2669d1512744f8f656dd1b0a52af6a9771be
Brian Manning <brian@xaoc.org>
2014-07-21 19:53:31 -0700

    qemu.ppc.sh: added back VNC switch with tweaks

--- qemu/qemu.ppc.sh
+++ qemu/qemu.ppc.sh
@@ -8,6 +8,9 @@ qemu-system-ppc \
     -pidfile ppc-base.pid \
     -serial telnet::4444,server,nowait \
     -localtime \
+    -vnc 127.0.0.1:1 \
+    -usbdevice tablet \
+    -monitor stdio \
     -redir tcp:28022::22 \
     -redir tcp:28000::8000 \
     -redir tcp:28200::4200 \
@@ -18,7 +21,7 @@ qemu-system-ppc \
     -hda ppc_disk.qcow2 \
     -cdrom /Users/Shared/Files/ISOs/debian-7.6.0-powerpc-CD-1.iso
 
-#    -vnc 127.0.0.1:1 \
+
 #    -append "root=/dev/mapper/mips_builder-root console=ttyS0"
 #    -cpu 7447A \
 #    -M mac99 \
