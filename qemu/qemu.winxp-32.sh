af5b116507fa6304dcb968db67abad16cdc4ed2f
Brian Manning <brian@xaoc.org>
2014-08-28 08:15:36 -0700

    qemu.winxp-32.sh: Added WinXP start script for QEMU 2.0.0

---
+++ qemu/qemu.winxp-32.sh
@@ -0,0 +1,26 @@
+PIDFILE=qemu.pid
+
+/usr/local/bin/qemu-system-i386 \
+    -m 1024 \
+    -name winxp-32 \
+    -pidfile $PIDFILE \
+    -localtime \
+    -monitor stdio \
+    -vnc 127.0.0.1:1 \
+    -k en-us \
+    -usb \
+    -usbdevice tablet \
+    -netdev user,id=foonet0 \
+    -device rtl8139,netdev=foonet0 \
+    -redir tcp:13389::3389 \
+    -redir tcp:13022::22 \
+    -cdrom winxpsp2.iso \
+    -boot order=cd,menu=on \
+    -hda disk1.qcow2
+
+# you can't use this if you background qemu, as the PID file will be removed
+# as soon as the shell returns from backgrounding the qemu process
+if [ $? -gt 0 ]; then
+   echo "QEMU exited with status code of $?"
+fi
+rm $PIDFILE
