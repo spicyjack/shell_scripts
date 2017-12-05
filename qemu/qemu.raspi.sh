c59effde474edfb95a280baf18bedfd694e4011d
Brian Manning <brian@xaoc.org>
2017-12-04 16:13:44 -0800

    qemu/qemu.raspi.sh: added updated copy of QEMU launcher script

--- qemu/qemu.raspi.sh
+++ qemu/qemu.raspi.sh
@@ -1,40 +1,23 @@
 #!/bin/sh
+IMG_FILE="2017-11-29-raspbian-stretch-lite.img"
 
 /usr/local/bin/qemu-system-arm \
     -name raspi \
-    -kernel raspi.kernel-qemu \
+    -kernel kernel-qemu-4.4.34-jessie \
     -cpu arm1176 \
     -m 256 \
     -M versatilepb \
     -no-reboot \
     -localtime \
     -monitor stdio \
-    -serial telnet::31023,server,nowait \
-    -redir tcp:31022::22 \
-    -redir tcp:31080::80 \
-    -redir tcp:31443::443 \
-    -hda 2012-10-28-wheezy-raspbian.img \
-    -hdb debian_wheezy_armhf.qcow2 \
-    -append "root=/dev/sda2 panic=1" \
+    -serial telnet::4023,server,nowait \
+    -net user,hostfwd=tcp::4022-:22 \
+    -net user,hostfwd=tcp::4080-:80 \
+    -net user,hostfwd=tcp::4443-:443 \
+    -drive file=${IMG_FILE},media=disk,format=raw \
+    -drive file=transfer_disk.img,media=disk,format=raw \
+    -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
 
-#    -redir tcp:29022::22 \
-#    -redir tcp:29000::8000 \
-#    -cdrom /Volumes/Raid/Users/brian/Files/ISOs/debian-testing-armhf-CD-1.iso \
-#    -serial telnet::4444,server,nowait \
-#    -vnc :1 -k en-us \
-#    -initrd initrd.img-2.6.26-1-versatile \
-#    -append "root=/dev/sda1"
+#    -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw init=/bin/bash" \
 
-# /usr/local/bin/qemu-system-arm \
-# -name "Raspberry Pi Emulator" \
-# -kernel kernel-qemu \
-# -cpu arm1176 \
-# -m 256 \
-# -M versatilepb \
-# -serial stdio \
-# -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
-# -drive file=2016-02-26-raspbian-jessie.img,index=0,media=disk,format=raw \
-# -net nic \
-# -net user,hostfwd=tcp::3122-:22,hostfwd=tcp::3180-:80
-
-# vim: set paste :
+# vim: set filetype=sh shiftwidth=2 tabstop=2 paste :
