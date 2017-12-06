4ead6b82b880200972319632e21b01e792a9f2de
Brian Manning <brian@xaoc.org>
2017-12-05 19:16:32 -0800

    qemu.raspi.nat.sh: don't use multiple hostfwd commands
    - Combined all of the 'hostfwd' params into a variable, and then use
      the variable when specifying what ports to forward in to the VM

--- qemu/qemu.raspi.nat.sh
+++ qemu/qemu.raspi.nat.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 IMG_FILE="2017-11-29-raspbian-stretch-lite.img"
 
+HOST_FWDS="hostfwd=tcp::4022-:22"
+HOST_FWDS="${HOST_FWDS},hostfwd=tcp::4080-:80"
+HOST_FWDS="${HOST_FWDS},hostfwd=tcp::4443-:443"
+
 /usr/local/bin/qemu-system-arm \
   -name raspi \
   -kernel kernel-qemu-4.4.34-jessie \
@@ -12,11 +16,9 @@ IMG_FILE="2017-11-29-raspbian-stretch-lite.img"
   -monitor stdio \
   -serial telnet::4023,server,nowait \
   -net nic \
-  -net user,hostfwd=tcp::4022-:22 \
-  -net user,hostfwd=tcp::4080-:80 \
-  -net user,hostfwd=tcp::4443-:443 \
+  -net user,${HOST_FWDS} \
   -drive file=${IMG_FILE},media=disk,format=raw \
-  -drive file=swap_disk.raw,media=disk,format=raw \
+  -drive file=swap_disk.img,media=disk,format=raw \
   -drive file=transfer_disk.img,media=disk,format=raw \
   -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
 
