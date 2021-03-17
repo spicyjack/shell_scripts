079df9e140a3f42640867ae0306150dabad68ce4
Brian Manning <brian@xaoc.org>
2021-03-17 08:42:08 -0700

    sqlite3.sh: updated paths, added check for SQLite dir

--- sqlite3.sh
+++ sqlite3.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Run SQLite3 from Homebrew
-SQLITE_VERSION="3.34.0"
+SQLITE_VERSION="3.34.1"
+SQLITE_DIR="/usr/local/Cellar/sqlite/${SQLITE_VERSION}"
 
-/usr/local/Cellar/sqlite/${SQLITE_VERSION}/bin/sqlite3 "$@"
+if [ ! -d $SQLITE_DIR ]; then
+  echo "ERROR: Missing SQLite folder; possible Homebrew upgrade?"
+  echo "ERROR: Searched: ${SQLITE_DIR}"
+  exit 1
+fi
+$SQLITE_DIR/bin/sqlite3 "$@"
 
 # vim: filetype=sh tabstop=2 shiftwidth=2
